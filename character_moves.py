767100356ece55ba203c612548cc8e76c123be4a
abg <kikceivieov@gmail.com>
2025-09-19 19:37:16 +0900

    top line 완성

--- character_moves.py
+++ character_moves.py
@@ -6,6 +6,8 @@ boy=load_image('character.png')
 
 def move_top():
     print("Move top")
+    for x in range(0,800,5):
+        draw_boy(x,550)
     pass
 
 def move_bottom():
@@ -34,16 +36,20 @@ def move_circle():
     print("Move circle")
     r = 200
     for deg in range(0,360):
-        x = r * math.cos(math.radians(deg)) +400
-        y = r * math.sin(math.radians(deg)) +300
-        clear_canvas_now()
-        boy.draw_now(x,y)
-        delay(0.1)
+        x = r * math.cos(math.radians(deg)) + 400
+        y = r * math.sin(math.radians(deg)) + 300
+        draw_boy(x, y)
     pass
 
 
+def draw_boy(x: float, y: float):
+    clear_canvas_now()
+    boy.draw_now(x, y)
+    delay(0.1)
+
+
 while True:
-    move_circle()
+    # move_circle()
     move_rectangle()
     break
     pass
